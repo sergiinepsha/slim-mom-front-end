a70d943ff11ca9486385b36cc93e14b7ce73962b
LeanNSP <nsp.lean@gmail.com>
2020-12-15 16:39:46 +0200

    clear App

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,7 @@
 import React from "react";
 
-import BasicButton from "./common/BasicButton/BasicButton";
-
 const App = () => {
-  return (
-    <>
-      <BasicButton name="Register" type="submit" />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
