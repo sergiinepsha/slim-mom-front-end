230cef29f72e712468bc195d08ea3596aceda2fb
LeanNSP <nsp.lean@gmail.com>
2020-12-15 16:33:41 +0200

    adds BasicButton

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import BasicButton from "./common/BasicButton/BasicButton";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <BasicButton name="Register" type="submit" />
+    </>
+  );
 };
 
 export default App;
