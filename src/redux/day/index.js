5b21d2da808993c51cac8b41ce7b7ff3cca04e15
LeanNSP <nsp.lean@gmail.com>
2020-12-15 13:07:27 +0200

    init redux

---
+++ src/redux/day/index.js
@@ -0,0 +1,4 @@
+export { default as dayActions } from "./dayActions";
+export { default as dayOperations } from "./dayOperations";
+export { default as dayReducer } from "./dayReducer";
+export { default as daySelectors } from "./daySelectors";
---
+++ src/routes/routes.js
@@ -0,0 +1,6 @@
+export default {
+  homePage: "/",
+  loginPage: "/login",
+  registerPage: "/register",
+  calculatorPage: "/calculator",
+};
