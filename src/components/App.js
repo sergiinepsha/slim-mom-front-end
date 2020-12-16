4625b0092eb8b80f10e46873804536a2edfd62de
LeanNSP <nsp.lean@gmail.com>
2020-12-16 22:09:26 +0200

    adds RegistrationForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './Header';
+import RegistrationForm from './RegistrationForm/RegistrationForm';
 
 const App = () => {
    return (
       <>
-         <Header />
+         <RegistrationForm />
       </>
    );
 };
