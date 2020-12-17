48b946d56ccdd90eb7fa8a1dfd0fa9ca60657f56
LeanNSP <nsp.lean@gmail.com>
2020-12-17 21:31:00 +0200

    adds RegisterPage

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './Header';
 import MainPage from './views/MainPage/MainPage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
 
 const App = () => {
    return (
       <>
          <Header />
-         <MainPage />
+         {/* <MainPage /> */}
+         <RegisterPage />
       </>
    );
 };
