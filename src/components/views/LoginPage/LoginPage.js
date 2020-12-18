96699a02cb17b8cb639807116be7d1526848417d
LeanNSP <nsp.lean@gmail.com>
2020-12-18 16:56:06 +0200

    adds bg img desctop

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -6,8 +6,11 @@ import s from './LoginPage.module.css';
 
 const LoginPage = () => {
    return (
-      <div className={s.container}>
-         <LoginForm />
+      <div className={s.wrapper}>
+         <img src="/images/desctop_min.png" alt="bg_img" className={s.bg_img} />
+         <div className={s.container}>
+            <LoginForm />
+         </div>
       </div>
    );
 };
