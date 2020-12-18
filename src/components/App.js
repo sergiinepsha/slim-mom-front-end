33ea1cb1dab95de6f6e2544622a5ece15d6fb704
SergiiPodlishchuk <podluy23@gmail.com>
2020-12-18 12:27:52 +0200

    diaryPage

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Header from './Header';
-import MainPage from './views/MainPage/MainPage';
+// import MainPage from './views/MainPage/MainPage';
+import DiaryPage from './views/DiaryPage/DiaryPage';
 
 const App = () => {
    return (
       <>
          <Header />
-         <MainPage />
+         {/* <MainPage /> */}
+         <DiaryPage />
       </>
    );
 };
