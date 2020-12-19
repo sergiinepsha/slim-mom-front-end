a7f0a05e59e47cc11d896c2701757e5e984d5a65
SergiiPodlishchuk <podluy23@gmail.com>
2020-12-19 21:24:47 +0200

    sd

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,11 +7,14 @@ import s from './modal.module.css';
 export default function Modal() {
    const closeModal = e => {
       e.preventDefault();
+
+      console.log(e);
+      console.log(e.target);
    };
 
    return (
       <>
-         <div className={s.overlay}>
+         <div className={s.overlay} onClick={closeModal}>
             <div className={s.container}>
                <button className={s.backButton} type="button" onClick={closeModal}></button>
                <button className={s.closeButton} type="button" onClick={closeModal}></button>
