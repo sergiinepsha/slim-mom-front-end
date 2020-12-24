b3afa35b16f635287938fe6c2e2bed84ccb3c474
BipRaider <thebipus@gmail.com>
2020-12-24 11:21:30 +0200

    Fix error in daile folder

--- src/redux/dailyRate/dailyRateActions.js
+++ src/redux/dailyRate/dailyRateActions.js
@@ -5,9 +5,9 @@ const getDailyIntakeRequest = createAction('dailyRate/fetchRequest');
 const getDailyIntakeSuccess = createAction('dailyRate/fetchSuccess');
 const getDailyIntakeError = createAction('dailyRate/fetchError');
 
-const getDailyIntake_ID_Request = createAction('dailyRate/fetchRequest');
-const getDailyIntake_ID_Success = createAction('dailyRate/fetchSuccess');
-const getDailyIntake_ID_Error = createAction('dailyRate/fetchError');
+const getDailyIntake_ID_Request = createAction('dailyRate/fetchIntake_ID_Request');
+const getDailyIntake_ID_Success = createAction('dailyRate/fetchIntake_ID_Success');
+const getDailyIntake_ID_Error = createAction('dailyRate/fetchIntake_ID_Error');
 
 export default {
    getDailyIntakeRequest,
