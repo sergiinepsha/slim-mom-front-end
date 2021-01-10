825081f274621b3d6f6762497da3cd40fe157316
BipRaider <thebipus@gmail.com>
2021-01-10 23:06:13 +0200

    added new styles  for  navgation item

--- src/components/presentational/Header/Navigation/Item/Item.jsx
+++ src/components/presentational/Header/Navigation/Item/Item.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import cn from 'classnames';
 
-import s from './Item.module.css';
+import styles from './Item.module.css';
 
 const Item = ({ to, name, children }) => {
+   const isAuth = useSelector(state => state.authUser.accessToken);
+
+   const authStyle = cn(styles.link, {
+      [styles.bars]: isAuth,
+   });
+
    return (
-      <li className={s.item}>
-         <NavLink exact to={to} className={s.link} activeClassName={s.activeLink}>
+      <li className={styles.item}>
+         <NavLink exact to={to} className={authStyle} activeClassName={styles.activeLink}>
             {name}
             {children}
          </NavLink>
