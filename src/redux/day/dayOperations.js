2dbcf813d8bbd87868ef5bf457df40ca1317af8e
LeanNSP <nsp.lean@gmail.com>
2021-01-12 09:05:38 +0200

    clearing dayOparations

--- src/redux/day/dayOperations.js
+++ src/redux/day/dayOperations.js
@@ -2,7 +2,6 @@
 import { dayActions } from './';
 import { userActions } from '../auth';
 import fetchDB from '../../services/fetchDB';
-import loaderActions from '../loader/loaderActions';
 
 const postEatenProduct = async (reqBody, dispatch) => {
    dispatch(dayActions.eatenProductRequest());
@@ -30,10 +29,8 @@ const deleteProduct = async (productAndDayIds, dispatch) => {
 
    try {
       const data = await fetchDB.del('/day', productAndDayIds);
-      console.log(data);
-      if (data) {
-         const daySummary = currentDaySummary(data);
 
+      if (data) {
          dispatch(dayActions.deleteEatenProductSuccess(eatenProductId));
          dispatch(dayActions.daySummary(data));
       }
@@ -52,22 +49,10 @@ const getInfoForDay = async (date, dispatch) => {
       const data = await fetchDB.post('/day/info', { date });
       const { _id, eatenProducts, daySummary, notAllowedProducts } = data;
 
-      // console.log(daySummary);
       dispatch(dayActions.daySummary(daySummary));
       dispatch(dayActions.dayId(_id));
       dispatch(dayActions.notAllowedProducts(notAllowedProducts));
-      // refact --v
-      if (!_id) {
-         // daySummarySetState({ ...data }, dispatch);
-         dispatch(dayActions.emptyEatenProducts());
-         dispatch(loaderActions.endLoader());
-         return;
-      }
-
-      // daySummarySetState(daySummary, dispatch);
-
-      eatenProductsSetState(eatenProducts, dispatch);
-      // ----^
+      dispatch(dayActions.eatenProductSuccess(eatenProducts));
    } catch (error) {
       if (error.response.status === 401) {
          dispatch(userActions.logoutUserSuccess());
@@ -75,31 +60,6 @@ const getInfoForDay = async (date, dispatch) => {
       dispatch(dayActions.infoForDayError(error));
    }
 };
-
-function currentDaySummary(data) {
-   if (data.daySummary) {
-      return data.daySummary;
-   }
-   if (data.newDaySummary) {
-      return data.newDaySummary;
-   }
-}
-
-function eatenProductsSetState(eatenProducts, dispatch) {
-   if (eatenProducts) {
-      dispatch(dayActions.eatenProductSuccess(eatenProducts));
-   } else {
-      dispatch(dayActions.emptyEatenProducts());
-   }
-}
-
-// function daySummarySetState(daySummary, dispatch) {
-//    if (daySummary) {
-//       dispatch(dayActions.daySummary(daySummary));
-//    } else {
-//       dispatch(dayActions.emptyDaySummary());
-//    }
-// }
 
 export default {
    postEatenProduct,
